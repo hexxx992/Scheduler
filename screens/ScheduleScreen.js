605058c052ad753ae961bc08b78e593cba25a050
Xi He <hexxx992@gmail.com>
2021-02-04 23:55:42 +0800

    edit screen

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,23 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
 import Banner from '../components/Banner';
 import CourseList from '../components/CourseList';
+import UserContext from '../components/UserContext';
+import CourseEditScreen from './CourseEditScreen';
 
 const ScheduleScreen = ({navigation}) => {
     const [schedule, setSchedule] = useState({ title: '', courses: [] });
-    
+    const user = useContext(UserContext);
+    const canEdit = user && user.role === 'admin';
     const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
     const view = (course) => {
-        navigation.navigate('CourseDetailScreen', { course });
-      };
+        navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
+    };
 
     useEffect(() => {
         const fetchSchedule =  async () => {
-        const response = await fetch(url);
-        if (!response.ok) throw response;
-        const json = await response.json();
-        setSchedule(json);
+            const response = await fetch(url);
+            if (!response.ok) throw response;
+            const json = await response.json();
+            setSchedule(json);
         }
         fetchSchedule();
     }, []);
